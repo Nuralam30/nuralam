384f9b59a821770e11a90ce3a9d49eeec94f4aa3
Nuralam30 <nuralam.hstu.930@gmail.com>
2023-04-10 16:52:21 +0600

    skills done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
 import aboutImage from '../../images/NurImage.png';
+import { skills } from './Skills';
 
 const About = () => {
 
@@ -10,46 +11,73 @@ const About = () => {
         const today = new Date().getTime();
         const birthday = new Date(date).getTime();
         const age_mili = Math.abs(today - birthday);
-    
+
         let days = Math.floor(age_mili / (1000 * 3600 * 24));
         let years = Math.floor(days / 365);
         days -= years * 365;
         let months = Math.floor(days / 31);
         days -= months * 31;
-    
+
         return `${years} years, ${months} months, ${days} days!`;
-      };
-    
+    };
+
     const age = calculateAge(birthDate);
 
 
     return (
         <div id='about'>
-            <div className="container row">
-                <div className="section-title mb-5">
-                    <h1>ABOUT</h1>
-                    <h4>ABOUT</h4>
-                </div>
-                <div className="col-md-6">
-                    <div className="about-image">
-                        <img src={aboutImage} alt="about-me-here" />
+            <div className="container">
+                <div className="row">
+                    <div className="section-title mb-5">
+                        <h1>ABOUT</h1>
+                        <h4>ABOUT</h4>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="about-image">
+                            <img src={aboutImage} alt="about-me-here" />
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="about-content">
+                            <p className='font-12 gray text-justify'>My name is <span className='font-14 blue'>Nura Alam</span>. Proffessionally, I am a UI/UX designer and Web Developer. I'm working on web design and development for more than 3 years.</p>
+
+                            <div className="row about-info mt-4 font-12 gray">
+                                <div className="col-md-6">
+                                    <p><span>Name: </span> Nura Alam</p>
+                                    <p><span>Age: </span> {age}</p>
+                                    <p><span>Nationality: </span> Bangladeshi</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <p><span>Freelance: </span> Available</p>
+                                    <p><span>Phone: </span> +8801854405107</p>
+                                    <p><span>Address: </span> Noadda, Daudkandi, Cumilla, Bangladesh</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-6">
-                    <div className="about-content">
-                        <p className='font-12 gray text-justify'>My name is <span className='font-14 blue'>Nura Alam</span>. Proffessionally, I am a UI/UX designer and Web Developer. I'm working on web design and development for more than 3 years.</p>
 
-                        <div className="row about-info mt-4 font-12 gray">
-                            <div className="col-md-6">
-                                <p><span>Name: </span> Nura Alam</p>
-                                <p><span>Age: </span> {age}</p>
-                                <p><span>Nationality: </span> Bangladeshi</p>
+                <div className="row mt-4">
+                    <div className="col-md-6">
+                        <div className="skills">
+                        <h3 className='sub-title mb-4'>Skills</h3>
+                        {
+                            skills.map(sk => <div className="skill mb-3">
+                            <div className="skill-content d-flex justify-content-between">
+                                <p>{sk.name}</p>
+                                <p>{sk.skill}</p>
                             </div>
-                            <div className="col-md-6">
-                                <p><span>Freelance: </span> Available</p>
-                                <p><span>Phone: </span> +8801854405107</p>
-                                <p><span>Address: </span> Noadda, Daudkandi, Cumilla, Bangladesh</p>
+                            <div className="skill-bar">
+                                <div className="bar" style={{width: sk.skill}}></div>
                             </div>
+                        </div>)
+                        }
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="education">
+                            <h3 className="sub-title mb-4">Education</h3>
                         </div>
                     </div>
                 </div>
---
+++ src/components/About/Skills.js
@@ -0,0 +1,27 @@
+
+export const skills = [
+    {
+        name: "HTML5 & CSS",
+        skill: "90%"
+    },
+    {
+        name: "JavaScript/TypeScript",
+        skill: "85%"
+    },
+    {
+        name: "REACT",
+        skill: "85%"
+    },
+    {
+        name: "Node JS",
+        skill: "70%"
+    },
+    {
+        name: "Next JS",
+        skill: "70%"
+    },
+    {
+        name: "PHP",
+        skill: "40%"
+    }
+]
